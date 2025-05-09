7733b5dd0ad16e3a2faaa09f812451ae661fa62b
Ruben-Garrido-Piquer <rubengarrido241@gmail.com>
2025-05-09 19:43:56 +0200

    Web + API

---
+++ assets/renombrar.bat
@@ -0,0 +1,11 @@
+@echo off
+rename "Akko.jpg" atsuko-kagari.jpg
+rename "sucy.jpg" sucy-manbavaran.jpg
+rename "lotte.jpg" lotte-yansson.jpg
+rename "amanda2.png" amanda-oneill.jpg
+rename "Constanze_Amalie.webp" constanze-amalie.jpg
+rename "jasminca.jpg" jasminka-antonenko.jpg
+rename "Diana_Cavendish_Ragnarok.webp" diana-cavendish.jpg
+rename "ursula.jpg" ursula-callistis.jpg
+rename "shiny chariot.jpg" shiny-chariot.jpg
+rename "croix.jpg" croix-meridies.jpg
